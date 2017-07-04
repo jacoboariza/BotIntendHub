22d32ddb68d815239bff8d5179f5e8767f2d1e30
Jacobo Ariza <luis.jacobo@gmail.com>
2017-07-04 19:28:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,20 @@
 #!/usr/bin/env python
-
-
-
 from __future__ import print_function
-
 from future.standard_library import install_aliases
-
 install_aliases()
-
-
-
 from urllib.parse import urlparse, urlencode
-
 from urllib.request import urlopen, Request
-
 from urllib.error import HTTPError
-
-
-
 import json
-
 import os
-
-
-
 from flask import Flask
-
 from flask import request
-
 from flask import make_response
-
-
-
 import yahooWeatherForecast
-
-
-
 makeYqlQuery = yahooWeatherForecast.makeYqlQuery
 
-
-
 # Flask app should start in global layout
-
 app = Flask(__name__)
-
-
-
-
 
 @app.route('/webhook', methods=['POST'])
 
