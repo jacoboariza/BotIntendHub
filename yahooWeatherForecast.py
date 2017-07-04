4fc4f3f6906dc18d5c1956e42ed4a149cae0f1eb
Jacobo Ariza <luis.jacobo@gmail.com>
2017-07-04 14:46:40 +0200

    Update yahooWeatherForecast.py

--- yahooWeatherForecast.py
+++ yahooWeatherForecast.py
@@ -1,1 +1,17 @@
 
+#!/usr/bin/env python
+
+
+def makeYqlQuery(req):
+
+    result = req.get("result")
+
+    parameters = result.get("parameters")
+
+    city = parameters.get("geo-city")
+
+    if city is None:
+
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='c'"
