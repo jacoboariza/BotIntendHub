2990d56cbc12052e02872a75cfee15c61b15929f
Jacobo Ariza <luis.jacobo@gmail.com>
2017-07-04 18:45:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-
-
-
-
 #!/usr/bin/env python
 
 from __future__ import print_function
@@ -22,11 +18,11 @@ from flask import make_response
 import yahooWeatherForecast
 
 makeYqlQuery = yahooWeatherForecast.makeYqlQuery
-
+#--------------------------------
 # Flask app should start in global layout
 app = Flask(__name__)
 
-
+#--------------------------------
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
@@ -41,8 +37,9 @@ def webhook():
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
+#--------------------------------
 
-
+#--------------------------------
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
@@ -55,35 +52,19 @@ def processRequest(req):
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
-
+#--------------------------------
 
 """
 def makeYqlQuery(req):
-
-
-
     result = req.get("result")
-
-
-
     parameters = result.get("parameters")
-
-
-
     city = parameters.get("geo-city")
-
-
-
     if city is None:
-
-
-
         return None
-
-
-
     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='c'"
 """
+
+#--------------------------------
 def makeWebhookResult(data):
     query = data.get('query')
     if query is None:
